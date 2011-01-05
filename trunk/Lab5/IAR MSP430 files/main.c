0ef8bed681a2da55c2caa1c3e8b2b1917c281133
abarcz <abarcz@f84a000c-9e2f-2036-cc63-9b3502332b91>
2011-01-05 11:33:23 +0000

    asm: zoptymalizowane przerwanie transmit (instrukcja DEC przesunięta ;])
    C: dodana sekcja error, wysyłająca komunikat o błędzie i restartująca system

--- trunk/Lab5/IAR MSP430 files/main.c
+++ trunk/Lab5/IAR MSP430 files/main.c
@@ -3,7 +3,7 @@
 #include "intrinsics.h"
 #include "string.h"         // memset
 
-#define BAUDRATE 115200      // baud. zejscie ponizej 31250 wymusza
+#define BAUDRATE 115200     // baud. zejscie ponizej 31250 wymusza
                             // wyliczanie U0BR1
 
 #define ACLK_FQ 8000000     // Hz
@@ -29,6 +29,8 @@ int main( void )
   
   // bufory I/O
   char buffer[BUF_SIZE];
+  char *err_buf = "MSP-ERROR: device reset.";
+  const int err_buf_size = strlen(err_buf);
   char i_char;                  // tymczasowy char do przechowania znaku
 
   int r_buf_pos = BUF_SIZE - 1; // pozycja kursora w buforze receive
@@ -41,7 +43,7 @@ int main( void )
   
   // wyzerowanie bufora I/O
   memset(buffer, 0, BUF_SIZE);
-  BCSCTL1 &= !BIT0;               // wlaczenie XT2
+  BCSCTL1 &= !BIT0;            // wlaczenie XT2 (XT2OFF = 0)
   BCSCTL1 |= RSEL0 + RSEL1 + RSEL2;
   
   /*
@@ -67,14 +69,14 @@ int main( void )
   Przygotowanie timera A.
   Timer A slu¿y do wykrycia idle line po zakoñczeniu odbioru znaków z URXD.
   */
-  TACTL |= 0x0200; //+ TAIE;     // wybranie SMCLK, w³ przerwania TAIFG
+  TACTL |= 0x0200;              // wybranie SMCLK
   TACCR0 = receive_timeout; 
   
   /*
   Przygotowanie USART w trybie UART
   */
   U0CTL |= SWRST;
-  U0CTL |= PENA  + PEV + SPB;         // 8bit + parzystosc (odd)
+  U0CTL |= PENA  + PEV + SPB;   // :TODO:
   U0TCTL |= SSEL1;              // wybranie SMCLK
   U0RCTL |= URXEIE;
   //:TODO: ewentualne budzenie procesora na rising edge receive
@@ -87,12 +89,10 @@ int main( void )
   // ew. dalsza czesc inicjalizacji tutaj
   
   U0CTL &= !SWRST;              // wyzerowanie SWRST
-  IE1 |= URXIE0;                 // wlacz przerwania receive
-  //IFG1 = 0xFF;
-  //TACCTL0 |= CCIE;      // wlacz przerwania
-  //TACTL |= MC_2;        // continous mode on
+  IE1 |= URXIE0;                // wlacz przerwania receive
 
   /***************** czêœæ aplikacyjna *****************/
+mainloop:
   while(1)
   {
     // Przejœcie w tryb uspienia + odlokowanie przerwañ
@@ -100,19 +100,21 @@ int main( void )
     __no_operation();
     
     /***************** odebranie znaku *****************/
-    TACCTL0 &= !(CCIE + CCIFG); // wylacz przerwania timeraA
+    TACCTL0 &= 0xFFEE;          // wylacz przerwania timeraA (CCIE, CCIFG)
     if (g_flags & BIT0)         // przerwanie receive zglasza dana do odebrania
     {
-      i_char = g_r_char;        // przepisanie znaku do bufora ap
-      g_flags &= !BIT0;         // wyczyszczenie znacznika oczekujacej danej
-      //r_buf_pos++;
+      /* sekcja krytyczna odczytu */
+      __disable_interrupt();    
+        i_char = g_r_char;        // przepisanie znaku do bufora ap
+        g_flags &= !BIT0;         // wyczyszczenie znacznika oczekujacej danej
+        TACTL &= 0xFFCF;          // wylacz timerA
+        TACCR0 = TAR + receive_timeout; // zapewnij przerwanie po rec_timeout
+      __enable_interrupt();
+      /* koniec sekcji krytycznej odczytu */
+      
       // jesli aplikacja nie nadaza z odbieraniem danych od przerwania, blad
       if (g_flags & BIT1)
-        goto error;
-      
-      TACTL &= 0xFFCF;           // wylacz timerA
-      TACCR0 = TAR + receive_timeout; // zapewnij przerwanie po rec_timeout
-      
+        goto error;      
       if (transmit_on == 1)     // nie mo¿e jednoczeœnie odbieraæ i wysy³aæ
         goto error;
       if (receive_on == 0)      // jest to poczatek odbierania
@@ -123,32 +125,30 @@ int main( void )
         TACCTL0 |= CCIE;        // wlacz przerwania
         TACTL |= MC_2;          // continous mode on
       }
-      //else
-      //{
-        if (i_char == 10) //10 = LF 13 = CR koniec odbioru
-        {
-          receive_on = 0;       // koniec odbierania
-          TACCR0 = receive_timeout; // zresetuj timer
-          TAR = 0;
-          ready_to_transmit = 1;
-        }
-        else if (r_buf_pos < 0)
-        {
-          goto error;
-        }
-        else                    // zapisz dana do bufora
-        {
-          buffer[r_buf_pos] = i_char;
-          r_buf_pos--;
-          TACCTL0 |= CCIE;      // wlacz przerwania
-          TACTL |= MC_2;        // continous mode on
-        }
-      //}
+      
+      if (i_char == 10)         //10 = LF 13 = CR koniec odbioru
+      {
+        receive_on = 0;         // koniec odbierania
+        TACCR0 = receive_timeout; // zresetuj timer
+        TAR = 0;
+        ready_to_transmit = 1;
+      }
+      else if (r_buf_pos < 0)
+      {
+        goto error;
+      }
+      else                    // zapisz dana do bufora
+      {
+        buffer[r_buf_pos] = i_char;
+        r_buf_pos--;
+        TACCTL0 |= CCIE;      // wlacz przerwania
+        TACTL |= MC_2;        // continous mode on
+      }
     }
     else if (g_flags & BIT5)    // timerA zglosil timeout, a receive milczy
     {
       receive_on = 0;           // koniec odbierania
-      TACTL &= 0xFFCF;           // wylacz i zresetuj timerA
+      TACTL &= 0xFFCF;          // wylacz i zresetuj timerA
       TACCR0 = receive_timeout;
       TAR = 0;
       
@@ -175,14 +175,51 @@ int main( void )
       transmit_on = 0;
       g_flags &= !BIT4;
       r_buf_pos = BUF_SIZE - 1;
-      IE1 &= 0x7F;           // wylacz przerwania transmit
-      //IE1
+      IE1 &= 0x7F;              // wylacz przerwania transmit
       IFG1 |= UTXIFG0;          // zapewnij zgloszenie sie transmit po 
                                 // odblokowaniu przerwania
     }
   }
   
-error:         
-  P1OUT |= BIT7;                // zapalenie diody bledu         
+  /*************** obs³uga b³êdu = komunikat + reset urz¹dzenia ***************/
+error:
+  __disable_interrupt();        // namieszalismy, trzeba posprzatac
+  P1OUT |= BIT7;                // zapalenie diody bledu    
+  
+  // wylacz i zresetuj timerA
+  TACTL &= 0xFFCF;              
+  TACCR0 = receive_timeout;
+  TAR = 0; 
+  TACCTL0 &= 0xFFEE;            // wylacz przerwania timeraA (CCIE, CCIFG)
+  
+  // reset zmiennych sterujacych
+  receive_on = 0;               
+  transmit_on = 0;
+  ready_to_transmit = 0;
+  r_buf_pos = BUF_SIZE - 1;
+  
+  // wylaczenie przerwan receive na czas wyslania komunikatu o bledzie
+  IE1 &= 0xBF;                  // wylacz przerwania receive
+  // przygotowanie do wyslania
+  g_flags = 0;
+  g_t_curr_char = err_buf;
+  g_t_chars_to_send = err_buf_size;
+  IFG1 |= UTXIFG0;              // zapewnij zgloszenie sie przerwania transmit
+  IE1 |= UTXIE0;                // wlacz przerwania transmit  
+  
+  // wyslanie komunikatu o bledzie
+  __enable_interrupt();
+  while (!(g_flags & BIT4))
+    ;
+  
+  __disable_interrupt();
+  IE1 &= 0x7F;                  // wylacz przerwania transmit
+  IE1 |= URXIE0;                // wlacz przerwania receive
+  IFG1 &= 0xBF;                 // skasowanie informacji o przerwaniu receive
+  IFG1 |= UTXIFG0;              // zapewnij zgloszenie sie przerwania transmit
+  g_flags = 0;
+  
+goto mainloop;                  // petla aplikacyjna odblokuje przerwania
+
   return 0;
 }
