70d2bf52eb142a8aeaceeaf7904bf3b66ca76e54
abarcz <abarcz@f84a000c-9e2f-2036-cc63-9b3502332b91>
2011-01-13 18:40:51 +0000

    Poprawiony błąd powodujący pisanie po pamięci. Refaktoryzacja przerwania receive i aplikacji. Komentarze.

--- Lab5/IAR MSP430 files/main.c
+++ Lab5/IAR MSP430 files/main.c
@@ -2,22 +2,26 @@
 #include "io430x16x.h"   
 #include "intrinsics.h"
 #include "string.h"           // memset
+#include "shared.h"           // wspolne definicje przerwan i aplikacji
 
 #define BAUDRATE 115200       // zejscie ponizej 31250 wymusza wyliczanie U0BR1
-#define SMCLK_FQ 8000000      // Hz
-#define BUF_NUM 2             // ilosc buforow IO
-#define BUF_SIZE 20           // rozmiar buforow odbiorczo-nadawczych
-#define LF 10
-#define CR 13
+#define SMCLK_FQ 8000000      // czestotliwosc SMCLK (ustawiany jako XT2)
+#define BUF_NUM 2             // ilosc buforow I/O
+#define LN_ENDING_CHARS 2     // ilosc znakow konczacych wysylana linie
+#define LF 10                 // znak LF
+#define CR 13                 // znak CR
 
 char *g_t_curr_char = NULL; // wskaznik nastepnego znaku do wyslania przez trans
-int g_t_chars_to_send = 0;  // ilosc znakow pozostala do wyslania przez trans
+int g_t_chars_count = 0;    // ilosc znakow pozostala do wyslania przez trans
 
 char *g_r_curr_char = NULL; // wskaznik miejsca na nastepny odebrany znak
-int g_r_chars_received = 0; // ilosc znakow odebrana przez receive
-int g_rec_addr_step = -1;
+int g_r_chars_count = 0;    // ilosc znakow odebrana przez receive
+int g_rec_addr_step = -1;   // krok w pamieci, o jaki nalezy zmienic adres
+                            // bufora odbiorczego po odebraniu znaku
 
-static char buffers[BUF_NUM][BUF_SIZE + 2];
+/* bufory I/O realizujace podwojne buforowanie */
+static char g_buffers[BUF_NUM][BUF_SIZE + LN_ENDING_CHARS];
+
 /* znaczniki przekazywane miedzy ISR a petla glowna
 b4 = przerwanie transmit zglosilo gotowosc do wyslania nowego tekstu
 b5 = przerwanie receive zglosilo koniec linii (odebrany wiersz)
@@ -25,27 +29,36 @@ b6 = przerwanie receive zlosilo przepelnienie bufora odbiorczego
 */
 int g_flags = 0; 
 
-int temp;
-
 int main( void )
 { 
   // Stop watchdog timer to prevent time out reset.
   WDTCTL = WDTPW + WDTHOLD;
   
   /***************** inicjalizacja systemu *****************/
-  // bufory I/O
-  char *err_buf1 = " MSP-ERROR: Line too long. Device reset. ";
-  char *err_buf2 = " MSP-ERROR: Requested second transfer. Device reset. ";
-  char *err_buf = err_buf1;
+   // wyzerowanie bufora I/O
+  memset(g_buffers, 0, (BUF_SIZE + LN_ENDING_CHARS) * BUF_NUM);
   
-  int receive_buffer = 0;     // indeks bufora odbiorczego (t_buf=(r_buf+1)%2)
-  int transmit_on = 0;        // jestesmy w trakcie wysylania wiersza
-
-  g_r_curr_char = buffers[receive_buffer] + BUF_SIZE - 1 - 2;
-  g_r_chars_received = 0;
+  // bufory I/O - ustawienie znakow konca linii dla wysylania
+  if (LN_ENDING_CHARS == 2)
+  {
+    g_buffers[0][BUF_SIZE + 1] = LF;
+    g_buffers[0][BUF_SIZE + 0] = CR;
+    g_buffers[1][BUF_SIZE + 1] = LF;
+    g_buffers[1][BUF_SIZE + 0] = CR;
+  }
   
-  // wyzerowanie bufora I/O
-  memset(buffers, 0, (BUF_SIZE + 2) * BUF_NUM);
+  int rec_buff_index = 0;       // indeks bufora odbiorczego (t_buf=(r_buf+1)%2)
+  int ongoing_trans = 0;        // czy jestesmy w trakcie wysylania wiersza
+  
+  // inicjalizacja bufora odbiorczego
+  g_r_curr_char = g_buffers[rec_buff_index] + BUF_SIZE - 1;
+  g_r_chars_count = 0;
+  
+  // bufory bledow
+  char *err_buf_overflow = " MSP-ERROR: Line too long. Device reset. ";
+  char *err_buf_sec_trans 
+    = " MSP-ERROR: Requested second transfer. Device reset. ";
+  char *err_buf = err_buf_overflow;
 
   /*
   Przygotowanie zegarów.
@@ -77,7 +90,6 @@ int main( void )
   IE1 |= URXIE0;                  // wlacz przerwania receive
 
   /***************** czêœæ aplikacyjna *****************/
-  g_r_chars_received = 0;
 mainloop:
   while(1)
   {
@@ -85,34 +97,38 @@ mainloop:
     __bis_SR_register(LPM0_bits + GIE);
     __no_operation();
     
-  mainloop_internal:
+mainloop_internal:
     /***************** zakonczenie odbioru *****************/
     if (g_flags & BIT5)         // zakonczenie odbioru (znak konca linii)
-    {                           // IE receive wylaczone
-        if (transmit_on)
+    {                           // przerwania receive sa wylaczone
+        /* jesli trwa wysylanie, nie mozemy uruchomic kolejnego - blad */
+        if (ongoing_trans)
         {
-          err_buf = err_buf2;
-          goto error;           // zakonczono kolejny odbior, gdy trwa transmit
+          err_buf = err_buf_sec_trans;  // ustaw komunikat bledu
+          goto error;           
         }
        
-        g_t_curr_char = buffers[receive_buffer] 
-            + (BUF_SIZE - g_r_chars_received - 2);
-        buffers[receive_buffer][BUF_SIZE - 1] = LF;
-        buffers[receive_buffer][BUF_SIZE - 2] = CR;
-        g_t_chars_to_send = g_r_chars_received + 2;
-
-        g_r_chars_received = 0;
-        receive_buffer = (receive_buffer + 1) % 2;  // przelaczenie buforow
-        g_r_curr_char = buffers[receive_buffer] + BUF_SIZE - 1 - 2;
-        transmit_on = 1;
+        /* przygotuj aktualny bufor odbiorczy jako bufor do wyslania */
+        g_t_curr_char = g_buffers[rec_buff_index] 
+            + (BUF_SIZE - g_r_chars_count);
+        g_t_chars_count = g_r_chars_count + LN_ENDING_CHARS;
+        
+        /* przygotuj drugi bufor jako bufor odbiorczy */
+        rec_buff_index = (rec_buff_index + 1) % 2;  
+        g_r_curr_char = g_buffers[rec_buff_index] + BUF_SIZE - 1;
+        g_r_chars_count = 0;
+        
         g_flags &= !BIT5;
-        IE1 |= UTXIE0 + URXIE0; // wlacz przerwania transmit aby wyslac
-    }                           // oraz przerwania receive - drugi bufor
+        
+        /* wlacz wysylanie z jednego bufora oraz odbieranie do drugiego */
+        ongoing_trans = 1;      // trwa wysylanie
+        IE1 |= UTXIE0 + URXIE0; // wlacz przerwania transmit i receive
+    }                           
     
     /**************** zakonczenie transmisji ****************/
     if (g_flags & BIT4)         // transmisja zakonczona
     {
-      transmit_on = 0;
+      ongoing_trans = 0;
       g_flags &= !BIT4;
       IE1 &= 0x7F;              // wylacz przerwania transmit
       IFG1 |= UTXIFG0;          // zapewnij zgloszenie sie transmit po 
@@ -122,15 +138,18 @@ mainloop:
     /****************** blad przepelnienia ******************/
     if (g_flags & BIT6)         // blad przepelnienia (IE receive wylaczone)
     {
-        err_buf = err_buf1;
+        err_buf = err_buf_overflow; // ustaw komunikat bledu
         goto error;             // wyslij informacje o bledzie
     }
     
     /************ sprawdz czy nie zglosilo sie kolejne przerwanie ************/
     __disable_interrupt();
-    if (g_flags != 0)           // cos przyszlo w trakcie petli glownej
-      goto mainloop_internal;   // musimy to obsluzyc zanim zasniemy
-  } // koniec petli glownej aplikacji
+      if (g_flags != 0)           // cos przyszlo w trakcie petli glownej
+      {
+        __enable_interrupt();
+        goto mainloop_internal;   // musimy to obsluzyc zanim zasniemy
+      }                           // petla glowna odblokuje przerwania
+  }                               // koniec petli glownej aplikacji
   
   /*************** obs³uga b³êdu = komunikat + reset urz¹dzenia ***************/
 error:
@@ -138,17 +157,17 @@ error:
   P1OUT |= BIT7;                // zapalenie diody bledu    
 
   // reset zmiennych sterujacych
-  receive_buffer = 0;              
-  transmit_on = 0;
-  g_r_curr_char = buffers[receive_buffer] + BUF_SIZE - 1 - 2;
-  g_r_chars_received = 0;
+  rec_buff_index = 0;              
+  ongoing_trans = 0;
+  g_r_curr_char = g_buffers[rec_buff_index] + BUF_SIZE - 1;
+  g_r_chars_count = 0;
   
   // wylaczenie przerwan receive na czas wyslania komunikatu o bledzie
   IE1 &= 0xBF;                  // wylacz przerwania receive
   // przygotowanie do wyslania
   g_flags = 0;
   g_t_curr_char = err_buf;
-  g_t_chars_to_send = strlen(err_buf);
+  g_t_chars_count = strlen(err_buf);
   
   IFG1 |= UTXIFG0;              // zapewnij zgloszenie sie przerwania transmit
   IE1 |= UTXIE0;                // wlacz przerwania transmit  
@@ -159,7 +178,7 @@ error:
     ;
   
   __disable_interrupt();
-  g_t_chars_to_send = 0;
+  g_t_chars_count = 0;
   IE1 &= 0x7F;                  // wylacz przerwania transmit
   IE1 |= URXIE0;                // wlacz przerwania receive
   IFG1 &= 0xBF;                 // skasowanie informacji o przerwaniu receive
---
+++ Lab5/IAR MSP430 files/shared.h
@@ -0,0 +1,5 @@
+
+/****************** wspolne definicje przerwan i aplikacji ******************/
+
+#define BUF_SIZE 20           // rozmiar buforow odbiorczo-nadawczych
+#define LINE_END 13           // znak konca linii odbierany przez port RS
